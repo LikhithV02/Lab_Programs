fc6e70dead776f767ee66b5d903ccad5dd072508
LikhithV02 <likhithv02@gmail.com>
2021-12-25 12:30:53 +0530

    6

--- Lab_Program_3.c
+++ Lab_Program_3.c
@@ -4,17 +4,18 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stdbool.h>
-#define STACK_SIZE 5
 
-#define CHOICE_PUSH  1
+#define CHOICE_PUSH 1
 #define CHOICE_POP 2
-#define CHOICE_PALINDROME  3
-#define CHOICE_  4
-#define CHOICE_EXIT    0
+#define CHOICE_PALINDROME 3
+#define CHOICE_ 4
+#define CHOICE_EXIT 0
 
-unsigned int menu() {
+unsigned int menu()
+{
   unsigned int choice = CHOICE_DELETE + 1;
-  while (choice > CHOICE_DELETE) {
+  while (choice > CHOICE_DELETE)
+  {
     printf("Enter your choice:\n");
     printf("\t %u - Create Array\n", CHOICE_CREATE);
     printf("\t %u - Display Array Elements\n", CHOICE_DISPLAY);
@@ -23,25 +24,27 @@ unsigned int menu() {
     printf("\t %u - Exit\n", CHOICE_EXIT);
     printf("\t choice: ");
     scanf("%d", &choice);
-    if (choice > CHOICE_DELETE) {
+    if (choice > CHOICE_DELETE)
+    {
       printf("ERROR: Invalid Choice\n");
     }
   }
   return choice;
 
-typedef struct {
-        size_t size;
-        int top;
-        int* arr;
-        }stack_t;
-
-  stack_t*  create(size_t size)
+  typedef struct
   {
-    stack_t* stack = malloc(sizeof(stack_t));
+    size_t size;
+    int top;
+    int *arr;
+  } stack_t;
+
+  stack_t *create(size_t size)
+  {
+    stack_t *stack = malloc(sizeof(stack_t));
     if (stack != NULL)
     {
       stack->size = size;
-      stack->arr = calloc(size , sizeof(int));
+      stack->arr = calloc(size, sizeof(int));
       if (stack->arr != NULL)
       {
         stack->top = -1; // stack is empty
@@ -49,106 +52,101 @@ typedef struct {
       else
       {
         free(stack);
-	stack = NULL;
+        stack = NULL;
       }
-    }  return stack;
+    }
+    return stack;
   }
 
-  bool  isfull(stack_t* s)
+  bool isfull(stack_t * s)
   {
     if (s->top == s->size)
     {
-       return true;
+      return true;
     }
     else
     {
-       return false;
+      return false;
     }
   }
 
-
-  int  isempty(stack_t* s)
+  int isempty(stack_t * s)
   {
-     if (s->top == -1)
-     {
-        return true;
-     }
-     else
-     {
-        return false;
-     }
-  }
-
-
-
-
-  int  push(stack_t* s, int elem)
-  {
-    if (isfull(s))    // is stack full?
+    if (s->top == -1)
     {
-       return 0;
+      return true;
     }
-    s->arr[++s->top]=elem;
+    else
+    {
+      return false;
+    }
+  }
+
+  int push(stack_t * s, int elem)
+  {
+    if (isfull(s)) // is stack full?
+    {
+      return 0;
+    }
+    s->arr[++s->top] = elem;
     return 1;
   }
 
-
-  int  pop(stack_t* s)
+  int pop(stack_t * s)
   {
-    if (isempty(s))  // is stack empty
+    if (isempty(s)) // is stack empty
     {
       return 0;
     }
     return (s->arr[s->top--]);
   }
 
-
-  bool is_palindrome(stack_t* s1 , int* arr , unsigned int mid)
+  bool is_palindrome(stack_t * s1, int *arr, unsigned int mid)
   {
-    while(s1->top>=0)
+    while (s1->top >= 0)
     {
-      if(pop(s1)!=arr[mid])
+      if (pop(s1) != arr[mid])
         return 0;
       else
-         mid++;
+        mid++;
     }
     return 1;
   }
 
-void main()
-{
-  stack_t* s1;         			//structure declaration 
-  unsigned int N,mid;  			//'N' is the maximum number of entries 
-  bool result;        			//a variable to store the result 
+  void main()
+  {
+    stack_t *s1;         // structure declaration
+    unsigned int N, mid; //'N' is the maximum number of entries
+    bool result;         // a variable to store the result
 
-  // taking the input of maximum number of entries from user
-  printf("Please enter the  number of digits::");
-  scanf("%d",&N);
+    // taking the input of maximum number of entries from user
+    printf("Please enter the  number of digits::");
+    scanf("%d", &N);
 
-  mid=N/2;
-  int arr[N];          			// an array to store the sequence of numbers
+    mid = N / 2;
+    int arr[N]; // an array to store the sequence of numbers
 
-  // taking a  series of  numbers as input
-  printf("Please enter a sequence of single digit  number::\n");
-  for(unsigned int i=0;i<N;i++)
-    scanf("%d",&arr[i]);
+    // taking a  series of  numbers as input
+    printf("Please enter a sequence of single digit  number::\n");
+    for (unsigned int i = 0; i < N; i++)
+      scanf("%d", &arr[i]);
 
-  s1=create(N/2);     			//to create a stucture which has the stack to store only the half series
+    s1 = create(N / 2); // to create a stucture which has the stack to store only the half series
 
-  for(unsigned int i=0;i<mid;i++)	// this pushes the only first half of series of numbers into the stack
-    push(s1 ,arr[i]);
+    for (unsigned int i = 0; i < mid; i++) // this pushes the only first half of series of numbers into the stack
+      push(s1, arr[i]);
 
-  if(N%2==0)
-     result = is_palindrome(s1 , arr , mid);
-  else
-  {					// condition to check wheather  maximum number of entries is odd or even
-     mid++;
-     result = is_palindrome(s1 , arr , mid);
+    if (N % 2 == 0)
+      result = is_palindrome(s1, arr, mid);
+    else
+    { // condition to check wheather  maximum number of entries is odd or even
+      mid++;
+      result = is_palindrome(s1, arr, mid);
+    }
+
+    if (result == 1)
+      printf("The entered sequense of numbers is palindrome!!\n");
+    else
+      printf("The entered sequense of numbers is not palindrome!!\n");
+    free(s1);
   }
-
-  if(result==1)
-   printf("The entered sequense of numbers is palindrome!!\n");
-  else
-   printf("The entered sequense of numbers is not palindrome!!\n");
-   free(s1);
-}
