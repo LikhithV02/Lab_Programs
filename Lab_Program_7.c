3ab57e22a21a5239aeddc9b6c29471d64ff76684
LikhithV02 <92736535+LikhithV02@users.noreply.github.com>
2022-04-18 01:24:13 +0530

    Update Lab_Program_7.c

--- Lab_Program_7.c
+++ Lab_Program_7.c
@@ -1,350 +1,241 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
-#define CHOICE_INSERT_AT_FRONT 1
-#define CHOICE_DELETE_AT_FRONT 2
-#define CHOICE_INSERT_AT_END 3
-#define CHOICE_DELETE_AT_END 4
-#define CHOICE_DISPLAY 5
-#define CHOICE_STACK_DEMO 6
-#define CHOICE_EXIT 7
+#define CHOICE_CREATE 1
+#define CHOICE_INSERT_FRONT 2
+#define CHOICE_DELETE_FRONT 3
+#define CHOICE_INSERT_END 4
+#define CHOICE_DELETE_END 5
+#define CHOICE_DISPLAY 6
+#define CHOICE_EXIT 0
 
-/**
- * This program implements a menu driven task to perform
- * operations such as insert node at front, delete node at front,
- * insert node at end, delete node at end, display linked list
- * elements and stack demonstration on singly linked list (SLL)
- * of Student Data with the fields : Name, USN, Branch, Sem, Phone number.
- **/
+typedef struct node{
+  char usn[10];
+  char name[20];
+  char branch[5];
+  unsigned short int sem;
+  char phno[10];
+  struct node* next_node;
+}node_t;
 
-unsigned int menu()
+typedef struct header{
+  node_t* front;
+  node_t* rear;
+}header_t;
+
+int menu()
 {
-  unsigned int choice = CHOICE_EXIT + 1;
-  while (choice > CHOICE_EXIT)
-  {
-    printf("\nEnter your choice:\n");
-    printf("\t %u - Insert at the beginning of the list\n", CHOICE_INSERT_AT_FRONT);
-    printf("\t %u - Delete at the beginning of the list\n", CHOICE_DELETE_AT_FRONT);
-    printf("\t %u - Insert at the end of the list\n", CHOICE_INSERT_AT_END);
-    printf("\t %u - Delete at the end of the list\n", CHOICE_DELETE_AT_END);
-    printf("\t %u - Display list Elements\n", CHOICE_DISPLAY);
-    printf("\t %u - Stack demonstration of SLL\n", CHOICE_STACK_DEMO);
-    printf("\t %u - Exit\n", CHOICE_EXIT);
-    printf("\t choice: ");
-    scanf("%d", &choice);
-    if (choice > CHOICE_EXIT)
-    {
-      printf("ERROR: Invalid Choice\n");
-    }
-  }
+  int choice;
+
+  printf("\n\t create list - %u\n",CHOICE_CREATE);
+  printf("\t to insert at the first - %u\n",CHOICE_INSERT_FRONT);
+  printf("\t to delete at the first - %u\n",CHOICE_DELETE_FRONT);
+  printf("\t to insert at the end - %u\n",CHOICE_INSERT_END);
+  printf("\t to delete at the end - %u\n",CHOICE_DELETE_END);
+  printf("\t to display list - %u\n",CHOICE_DISPLAY);
+  printf("\t to exit - %u\n",CHOICE_EXIT);
+  printf("\t\t PLEASE ENTER YOUR CHOISE : ");
+  scanf("%d",&choice);
+  //getchar();
+
   return choice;
 }
 
-/**
- * Initialising the struct
- **/
 
-typedef struct listnode
+/*
+* create function
+*/
+header_t* create(header_t* hdr)
 {
-  char name[50];
-  char branch[10];
-  char USN[10];
-  char phno[10];
-  int sem;
-  struct listnode *link;
-} listnode_t;
+  hdr =(header_t*)malloc(sizeof(header_t));
+  hdr->front = NULL;
+  hdr->rear = NULL;
 
-listnode_t *create();
-listnode_t *insert_at_front(listnode_t *list);
-listnode_t *delete_at_front(listnode_t *list);
-listnode_t *insert_at_end(listnode_t *list);
-listnode_t *delete_at_end(listnode_t *list);
-void display(listnode_t *list);
-void fn_exit(listnode_t *list);
-void stack_demo();
-
-/**
- * Function to create a node
- **/
-
-listnode_t *create()
-{
-  char name[50];
-  char branch[10];
-  char USN[10];
-  char phno[10];
-  int sem;
-
-  listnode_t *node = (listnode_t *)malloc(sizeof(listnode_t));
-
-  printf("\t Student name : ");
-  scanf("%s", name);
-  strncpy(node->name, name, 50);
-
-  printf("\t Student USN : ");
-  scanf("%s", USN);
-  strncpy(node->USN, USN, 10);
-
-  printf("\t Student branch : ");
-  scanf("%s", branch);
-  strncpy(node->branch, branch, 10);
-
-  printf("\t Student sem : ");
-  scanf("%d", &sem);
-  node->sem = sem;
-
-  printf("\t Student phone number : ");
-  scanf("%s", phno);
-  strncpy(node->phno, phno, 10);
-
-  node->link = NULL;
-
-  return node;
+  return hdr;
 }
 
-/**
- * Function to insert a node at the front of the list
- **/
 
-listnode_t *insert_at_front(listnode_t *list)
+/*
+* insert function
+*/
+header_t* insert(header_t* hdr , int choice)
 {
-  listnode_t *temp = NULL;
+  node_t* node;
 
-  if (list == NULL)
+  node = (node_t*)malloc(sizeof(node_t));
+
+  printf("\t enter the usn : ");
+  //fgets(node->usn ,10 , stdin);
+  scanf("%s",node->usn);
+
+  printf("\t enter the name : ");
+  //fgets(node->name , 20 , stdin);
+  scanf("%s",node->name);
+  //getchar();
+
+  printf("\t enter the branch : ");
+  //fgets(node->branch , 5 , stdin);
+  scanf("%s",node->branch);
+
+  printf("\t enter the semester : ");
+  scanf("%hu",&node->sem);
+  //getchar();
+
+  printf("\t enter the phone number : ");
+  //fgets(node->phno , 10 , stdin);
+  scanf("%s",node->phno);
+
+  node->next_node = NULL;
+
+  if(hdr->front==NULL)
   {
-    list = create();
-    return list;
+    hdr->front = node;
+    hdr->rear = node;
   }
   else
   {
-    temp = create();
-    temp->link = list;
-    list = temp;
-    return list;
-  }
-}
-
-/**
- * Function to delete a node at the front of the list
- **/
-
-listnode_t *delete_at_front(listnode_t *list)
-{
-  listnode_t *temp = NULL;
-
-  if (list == NULL)
-  {
-    printf("List is empty\n");
-    return NULL;
-  }
-  else
-  {
-    if (list->link == NULL)
+    if(choice == CHOICE_INSERT_FRONT)
     {
-      printf("\n\t %s's data has been deleted", list->name);
-      free(list);
-      return NULL;
+      node->next_node = hdr->front;
+      hdr->front = node;
+      //printf("1\n");
     }
     else
     {
-      temp = list;
-      list = list->link;
-      printf("\n\t %s's data has been deleted", temp->name);
-      free(temp);
-      return list;
+      hdr->rear->next_node = node;
+      hdr->rear = node;
+      //printf("0\n");
     }
   }
+  return hdr;
 }
 
-/**
- * Function to insert a node at the end of the list
- **/
 
-listnode_t *insert_at_end(listnode_t *list)
+/*
+* delete function
+*/
+
+header_t* deletee(header_t* hdr , int choice)
 {
-  listnode_t *temp = NULL;
-  listnode_t *last = NULL;
-
-  if (list == NULL)
+if(hdr->front!=NULL)
+{
+  if(choice==CHOICE_DELETE_FRONT)
   {
-    list = create();
-    return list;
+    hdr->front = hdr->front->next_node;
   }
   else
   {
-    temp = create();
-    last = list;
-    while (last->link != NULL)
+    node_t* prev;
+    node_t* curr;
+    prev=hdr->front;
+    curr=prev->next_node;
+
+    if(curr==NULL)
     {
-      last = last->link;
-    }
-    last->link = temp;
-    return list;
-  }
-}
-
-/**
- * Function to delete a node at the end of the list
- **/
-
-listnode_t *delete_at_end(listnode_t *list)
-{
-  listnode_t *temp = NULL;
-  listnode_t *last = NULL;
-
-  if (list == NULL)
-  {
-    printf("List is empty\n");
-    return NULL;
-  }
-  else
-  {
-    if (list->link == NULL)
-    {
-      printf("\n\t %s's data has been deleted", list->name);
-      free(list);
-      return NULL;
+      hdr->front=NULL;
+      hdr->rear=NULL;
+      return hdr;
     }
     else
     {
-      temp = list;
-      while (temp->link != NULL)
+      while(curr->next_node!=NULL)
       {
-        last = temp;
-        temp = temp->link;
+        prev=curr;
+        curr=curr->next_node;
       }
-      printf("\n\t %s's data has been deleted", temp->name);
-      free(temp);
-      last->link = NULL;
-      return list;
+
+      prev->next_node=NULL;
+      hdr->rear=prev;
+      free(curr);
     }
   }
 }
+else
+  printf("there is no node inserted , please insert a node first\n");
 
-/**
- * Function to show stack demonstration of SLL
- **/
+return hdr;
+}
 
-void stack_demo()
+/*
+*
+*/
+
+void display(header_t* hdr)
 {
-  size_t choice;
-  listnode_t *stack = NULL;
-
-  while (true)
+  int cnt=1;
+  node_t* temp;
+  temp=hdr->front;
+  while(temp!=NULL)
   {
-    printf("\n Stack demo of SLL \n");
-    printf("\t 1 - Push an element onto Stack\n");
-    printf("\t 2 - Pop an element from Stack\n");
-    printf("\t 3 - Display stack elements\n");
-    printf("\t 4 - Exit\n");
-    printf("\t choice : ");
-    scanf("%zu", &choice);
-    if (choice > 4)
-    {
-      printf("ERROR: Invalid Choice\n");
-    }
+    printf("student %d info :\n",cnt);
+    printf("\tusn - %s\n",temp->usn);//fputs(temp->usn , stdout);
+    printf("\tname - %s\n",temp->name);//fputs(temp->name , stdout);
+    printf("\tbranch - %s\n",temp->branch);//fputs(temp->branch , stdout);
+    printf("\tsemester - %d\n",temp->sem);
+    printf("\tphone number - %s\n",temp->phno);//fputs(temp->phno , stdout);
+    cnt++;
 
-    switch (choice)
-    {
-    case 1:
-      stack = insert_at_front(stack);
-      break;
-    case 2:
-      stack = delete_at_front(stack);
-      break;
-    case 3:
-      display(stack);
-      break;
-    case 4:
-      fn_exit(stack);
-      return;
-    default:
-      return;
-    }
+    temp=temp->next_node;
   }
+  if(hdr->front==NULL)
+    printf("there is no student data to display\nplease enter a student data by choosing insert from menu\n");
+
 }
 
-/**
- * Function to display nodes of singly linked list
- **/
-
-void display(listnode_t *list)
-{
-  listnode_t *temp = list;
-  int count = 0;
-
-  if (temp == NULL)
-  {
-    printf("List is empty\n");
-  }
-  else
-  {
-    while (temp != NULL)
-    {
-      printf("\n\t Student name : %s", temp->name);
-      printf("\n\t Student USN : %s", temp->USN);
-      printf("\n\t Student branch : %s", temp->branch);
-      printf("\n\t Student sem : %d", temp->sem);
-      printf("\n\t Student phone number : %s\n", temp->phno);
-      temp = temp->link;
-      count++;
-    }
-    printf("\n\t Number of students = %d ", count);
-  }
-}
-
-/**
- * Function to free memory allocated to SLL
- **/
-
-void fn_exit(listnode_t *list)
-{
-  if (list != NULL)
-  {
-    free(list);
-    printf("Memory allocated to the list has been released\n");
-  }
-}
 
 int main()
 {
-  size_t choice;
-  listnode_t *list = NULL;
-
-  while (true)
+  int choice;
+  header_t* hdr=NULL;
+  while(1)
   {
-    choice = menu();
+    choice=menu();
     switch (choice)
     {
-    case CHOICE_INSERT_AT_FRONT:
-      list = insert_at_front(list);
-      break;
-    case CHOICE_DELETE_AT_FRONT:
-      list = delete_at_front(list);
-      break;
-    case CHOICE_INSERT_AT_END:
-      list = insert_at_end(list);
-      break;
-    case CHOICE_DELETE_AT_END:
-      list = delete_at_end(list);
-      break;
-    case CHOICE_DISPLAY:
-      display(list);
-      break;
-    case CHOICE_STACK_DEMO:
-      stack_demo();
-      break;
-    case CHOICE_EXIT:
-      fn_exit(list);
-      exit(0);
-      break;
-    default:
-      fn_exit(list);
-      list = NULL;
-      exit(0);
-      break;
+      case CHOICE_CREATE:
+	if(hdr==NULL)
+	{
+	  hdr = create(hdr);
+	  printf("please choose insert :\n");
+	}
+	else
+	  printf("there is already a list created\n");
+	break;
+
+      case CHOICE_INSERT_FRONT:
+	if(hdr==NULL)
+	  hdr = create(hdr);
+	hdr = insert(hdr , CHOICE_INSERT_FRONT);
+	printf("a node is inserted at front :\n");
+	break;
+
+      case CHOICE_INSERT_END:
+	if(hdr==NULL)
+	  hdr = create(hdr);
+	hdr = insert(hdr , CHOICE_INSERT_END);
+        printf("\t\ta node is inserted at end :\n");
+        break;
+
+      case CHOICE_DELETE_FRONT:
+	hdr = deletee(hdr , CHOICE_DELETE_FRONT);
+        printf("\t\ta node is deleted at front :\n");
+	break;
+
+      case CHOICE_DELETE_END:
+	hdr = deletee(hdr , CHOICE_DELETE_END);
+        printf("\t\ta node is deleted at end :\n");
+        break;
+
+      case CHOICE_DISPLAY:
+	display(hdr);
+	break;
+
+      case CHOICE_EXIT:
+	free(hdr);
+	exit(0);
     }
+
   }
+  return 0;
 }
